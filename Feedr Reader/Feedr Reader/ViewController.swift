fdce4870deec07a16fcf202b16bac5bde5820995
Israel Manzo <israman814@gmail.com>
2016-12-07 14:41:33 -0500

    adding new files

---
+++ Feedr Reader/Feedr Reader/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Feedr Reader
+//
+//  Created by Israel Manzo on 12/6/16.
+//  Copyright © 2016 Israel Manzo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for:indexPath)
+        return cell    }
+
+}
+
