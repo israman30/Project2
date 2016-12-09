0b26537b3aa026f242f4784a97063769c55be074
Israel Manzo <israman814@gmail.com>
2016-12-08 20:49:28 -0500

    updating README

--- Feedr Reader/Feedr Reader/ArticleData.swift
+++ Feedr Reader/Feedr Reader/ArticleData.swift
@@ -8,16 +8,52 @@
 
 import Foundation
 
-class ArticleData {
+class Articles {
+    var author: [String]?
+    var title: [String]?
+    var description: [String]?
+    init(article: [String : Any]) {
     
-    func fetchData(closure: @escaping (Data) -> ()) {
+        author = article["author"] as? [String]
+        title = article["title"] as? [String]
+        description = article["description"] as? [String]
+    }
+
+}
+
+class NewsApiResponse {
+    var status : String?
+    var source : String?
+    var sortBy: String?
+    var articles: [Articles]?
+    
+    init(json: [String : Any]) {
+        status = json["status"] as? String
+        source = json["source"] as? String
+        sortBy = json["sortBy"] as? String
         
-        let endpoint = "https://www.googleapis.com/books/v1/volumes?q=isbn:055380457X"
+        articles = []
+    
+        if let articlesJson = json["articles"] as? [[String : Any]] {
+            for articleJason in articlesJson {
+                articles?.append(Articles(article: articleJason))
+            }
+        }
         
+    }
+}
+
+class ArticleData {
+   
+    static func fetchData(closure: @escaping (Data) -> ()) {
+        
+        let endpoint = "https://newsapi.org/v1/articles?source=techcrunch&apiKey=066d82458ed84eeeac28a86095ec88b9"
         let url = URLRequest(url: URL(string: endpoint)!)
         let session = URLSession(configuration: URLSessionConfiguration.default)
         let task = session.dataTask(with: url) { data, _ , _  in
             let json = try! JSONSerialization.jsonObject(with: data!, options: .mutableContainers) as! [String : Any]
+            
+            let parsedResponse = NewsApiResponse(json: json)
             
             guard let responseData = data else {
                 print("Error: did not receive data")
